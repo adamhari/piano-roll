2040425a246ba7cf8aff80ea54db5a8421b0e77d
Adam Hari <adamhari25@hotmail.com>
2020-02-06 20:21:36 -0700

    Updates

---
+++ precache-manifest.2dd766e30d80878439e326b66b652381.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c33ffe9ff2076da21602b0418e7d42",
+    "url": "/piano-roll/index.html"
+  },
+  {
+    "revision": "366be460ffe20ba45da8",
+    "url": "/piano-roll/static/css/main.6c81c0a3.chunk.css"
+  },
+  {
+    "revision": "0b414ce908feae3e6549",
+    "url": "/piano-roll/static/js/2.0ce97aab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/piano-roll/static/js/2.0ce97aab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "366be460ffe20ba45da8",
+    "url": "/piano-roll/static/js/main.86f1965b.chunk.js"
+  },
+  {
+    "revision": "f32ad4936a85c55a29ba",
+    "url": "/piano-roll/static/js/runtime-main.585bb622.js"
+  },
+  {
+    "revision": "1c034a3c79b7bb221891bf22588b450d",
+    "url": "/piano-roll/static/media/AVENGEANCE HEROIC AVENGER.1c034a3c.ttf"
+  },
+  {
+    "revision": "223f224650510797e0f06233d2a0f97b",
+    "url": "/piano-roll/static/media/DS-DIGIT.223f2246.TTF"
+  },
+  {
+    "revision": "22432e609f3d330163a4bfe3a63f00e7",
+    "url": "/piano-roll/static/media/Russo_One.22432e60.ttf"
+  }
+]);

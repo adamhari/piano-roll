2b6255ce547f5c4b676a42bb7e0238a17e36f157
Adam Hari <adamhari25@hotmail.com>
2020-04-09 14:56:56 -0600

    remove deprecated Waveform mic code tweak instr-right-top flex alignment

--- src/components/Waveform.jsx
+++ src/components/Waveform.jsx
@@ -1,27 +1,8 @@
 import React, {useEffect, useRef} from 'react';
-import {usePrevious} from '../js/hooks';
 import WaveSurfer from 'wavesurfer.js';
-import MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone';
 
-const Waveform = ({audio, handleClickControl, recording}) => {
+const Waveform = ({audio}) => {
 	const wavesurfer = useRef(null);
-
-	// const recordingChunks = useRef([]);
-	// const prevRecording = usePrevious(recording);
-
-	// useEffect(() => {
-	// 	const stopRecording = () => {
-	// 		console.log('sup', recordingChunks.current);
-	// 		wavesurfer.current.microphone.stop();
-	// 		const microphoneAudioBlob = new Blob(recordingChunks.current);
-	// 		const microphoneAudioUrl = URL.createObjectURL(microphoneAudioBlob);
-	// 		const microphoneAudio = new Audio(microphoneAudioUrl);
-	// 		handleClickControl('sample', microphoneAudio);
-	// 	};
-
-	// 	recording && !prevRecording && wavesurfer.current.microphone.start();
-	// 	!recording && prevRecording && stopRecording();
-	// }, [handleClickControl, recording, prevRecording]);
 
 	useEffect(() => {
 		wavesurfer.current = WaveSurfer.create({
@@ -30,34 +11,13 @@ const Waveform = ({audio, handleClickControl, recording}) => {
 			// barHeight: 3,
 			// barRadius: 3,
 			// barWidth: 3,
-			container: '.waveform',
+			container: '#waveform',
 			cursorWidth: 0,
 			hideScrollbar: true,
 			normalize: true,
 			interact: false,
 			waveColor: '#ff2601',
-			// plugins: [MicrophonePlugin.create()],
 		});
-
-		// wavesurfer.current.microphone.on('deviceReady', (stream) => {
-		// 	console.log('yo', stream);
-		// 	recordingChunks.current = [];
-		// 	const mediaRecorder = new MediaRecorder(stream);
-		// 	mediaRecorder.ondataavailable = (e) => {
-		// 		console.log('da', e);
-		// 		recordingChunks.current.push(e.data);
-		// 	};
-
-		// 	mediaRecorder.onstop = (e) => {
-		// 		console.log('stop');
-		// 	};
-
-		// 	mediaRecorder.start();
-		// });
-
-		// 	wavesurfer.current.microphone.on('deviceError', (e) => {
-		// 		console.log('wavesurfer microphone deviceError: ', e);
-		// 	});
 
 		return () => wavesurfer.current.destroy();
 	}, []);

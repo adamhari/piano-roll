a081278d1ea0f1755f99cd373a5be7c213575060
Adam Hari <adamhari25@hotmail.com>
2020-09-04 11:06:12 -0600

    MidiInputDropdown - change renderer to return an empty select when no inputs are detected

--- src/components/MidiInputDropdown.tsx
+++ src/components/MidiInputDropdown.tsx
@@ -33,27 +33,34 @@ const MidiInputDropdown = ({handleMidiKeyDown, handleMidiKeyUp}: Props) => {
 		setActiveInputName(selectedInput?.name);
 	};
 
-	if (inputs.length > 0) {
-		return (
-			<div id='midi-input-container'>
-				<svg id='midi-input-icon' viewBox='-50 -30 200 200'>
-					<path
-						fill='#adadad'
-						d='M122.88,35.289L87.945,70.578v-17.58c-22.091-4.577-39.542,0.468-52.796,17.271 c2.301-34.558,25.907-51.235,52.795-52.339L87.945,0L122.88,35.289L122.88,35.289z'
-					/>
-					<path
-						fill='#adadad'
-						d='M6.908,23.746h35.626c-4.587,3.96-8.71,8.563-12.264,13.815H13.815v62.943h80.603V85.831l13.814-13.579v35.159 c0,3.814-3.093,6.907-6.907,6.907H6.908c-3.815,0-6.908-3.093-6.908-6.907V30.653C0,26.838,3.093,23.746,6.908,23.746L6.908,23.746 z'
-					/>
-				</svg>
-				<select value={activeInputName} onChange={handleChange} className={'midi-input-dropdown'}>
-					{inputs.map((x, i) => (
-						<option key={i}>{x.name}</option>
-					))}
-				</select>
-			</div>
+	const renderIcon = () => (
+		<svg id='midi-input-icon' viewBox='-50 -30 200 200'>
+			<path
+				fill='#adadad'
+				d='M122.88,35.289L87.945,70.578v-17.58c-22.091-4.577-39.542,0.468-52.796,17.271 c2.301-34.558,25.907-51.235,52.795-52.339L87.945,0L122.88,35.289L122.88,35.289z'
+			/>
+			<path
+				fill='#adadad'
+				d='M6.908,23.746h35.626c-4.587,3.96-8.71,8.563-12.264,13.815H13.815v62.943h80.603V85.831l13.814-13.579v35.159 c0,3.814-3.093,6.907-6.907,6.907H6.908c-3.815,0-6.908-3.093-6.908-6.907V30.653C0,26.838,3.093,23.746,6.908,23.746L6.908,23.746 z'
+			/>
+		</svg>
+	);
+
+	const renderOptions = () =>
+		inputs.length > 0 ? (
+			inputs.map((x, i) => <option key={i}>{x.name}</option>)
+		) : (
+			<option>No MIDI Devices Found</option>
 		);
-	} else return null;
+
+	return (
+		<div id='midi-input-container'>
+			{renderIcon()}
+			<select value={activeInputName} onChange={handleChange} className={'midi-input-dropdown'}>
+				{renderOptions()}
+			</select>
+		</div>
+	);
 };
 
 export default MidiInputDropdown;

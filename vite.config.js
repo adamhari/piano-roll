c8faba661c5f6b1b96062b8beccad9ace47f8b68
Adam Hari <adamjhari@outlook.com>
2025-01-05 17:57:42 -0700

    build: fix deployment base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   assetsInclude: "**/*.TTF",
+  base: "/piano-roll/",
   plugins: [react()],
 });
